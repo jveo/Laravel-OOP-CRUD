ed0dc367ed4d8d0cc02103156767a4f2dca819e4
Jesse Viau <jesse.viau92@stclairconnect.ca>
2021-02-01 17:05:19 -0500

    Init Commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function (){
+    return "<h1>Jesse Viau</h1>";
+});
+
+Route::get('/search', function (){
+    return "<h1>Laravel makes it easy to develop websites</h1>";
+});
+
+Route::get('/uid/{id}', function ($id){
+    return "ID: $id";
+})->where('id', '[0-9]+');
+
+
+Route::group(['as'=>'users.', 'prefix'=>'users'], function (){
+
+
+    Route::get('/{user?}', function ($user = "Batman"){
+        return $user;
+    })->name('show');
+
+    Route::get('/{user}/images/{id}', function ($user, $id){
+        return "Name: $user \nImage: $id";
+    })->where('images', '[0-9]+')->name('images.show');
+
+
+});
+
+
